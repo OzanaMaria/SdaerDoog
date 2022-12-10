e4d1da9c9f110abb8d47b84d474711cb3295926b
Gabriel CROSMAN (94486) <gabriel.crosman@stud.acs.upb.ro>
2022-12-10 12:14:08 +0200

    schelet backend

---
+++ backend/src/test/java/com/sdaerdoog/sdaerdoog/SdaerdoogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdaerdoog.sdaerdoog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaerdoogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
